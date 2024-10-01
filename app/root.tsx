7fb52a03525aa95bf3309d966fc1b4e8e0acdf3f
Simon Escobar Benitez <sescobb27@gmail.com>
2024-10-01 17:59:10 -0500

    remove unused code and add buy me a coffee

--- app/root.tsx
+++ app/root.tsx
@@ -35,7 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <body>
         {children}
         <ScrollRestoration />
-        <Scripts />
+        <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="kirobyte" data-color="#FFDD00" data-emoji="🍻" data-font="Cookie" data-text="Buy me a beer" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
       </body>
     </html>
   );
